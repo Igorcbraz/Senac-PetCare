fbb1d5a02cf986cc012b9218644f6c904b1b6698
Igor Costa Braz <Igorcbraz1@gmail.com>
2025-05-20 08:13:53 -0300

    fix: adjust component name to build

--- src/components/NotificacoesComponent.vue
+++ src/components/NotificacoesComponent.vue
@@ -1,20 +1,13 @@
 <template>
   <div>
-    <q-btn
-      flat
-      round
-      dense
-      icon="notifications"
-      @click="menu = !menu"
-      color="grey-8"
-    >
+    <q-btn flat round dense icon="notifications" @click="menu = !menu" color="grey-8">
       <q-badge v-if="reminders.length" color="red" floating>
         {{ reminders.length }}
       </q-badge>
     </q-btn>
 
     <q-menu v-model="menu" anchor="bottom right" self="top right" :offset="[0, 10]">
-      <q-list style="min-width: 250px; max-width: 300px;">
+      <q-list style="min-width: 250px; max-width: 300px">
         <q-item v-for="(item, index) in limitedReminders" :key="index" clickable>
           <q-item-section>
             <q-item-label>{{ item.title }}</q-item-label>
@@ -25,9 +18,7 @@
         <q-separator />
 
         <q-item clickable @click="goToReminders">
-          <q-item-section class="text-primary text-center">
-            Ver todas
-          </q-item-section>
+          <q-item-section class="text-primary text-center"> Ver todas </q-item-section>
         </q-item>
       </q-list>
     </q-menu>
@@ -36,7 +27,7 @@
 
 <script>
 export default {
-  name: 'Notificacoes',
+  name: 'NotificacoesComponent',
   data() {
     return {
       menu: false,
@@ -44,19 +35,19 @@ export default {
         { title: 'Vacina do pet', date: '20/05/2025' },
         { title: 'Consulta marcada', date: '22/05/2025' },
         { title: 'Revisão do cadastro', date: '23/05/2025' },
-        { title: 'Plano prestes a vencer', date: '25/05/2025' }
-      ]
-    };
+        { title: 'Plano prestes a vencer', date: '25/05/2025' },
+      ],
+    }
   },
   computed: {
     limitedReminders() {
-      return this.reminders.slice(0, 3);
-    }
+      return this.reminders.slice(0, 3)
+    },
   },
   methods: {
     goToReminders() {
-      this.$router.push('/reminders');
-    }
-  }
-};
+      this.$router.push('/reminders')
+    },
+  },
+}
 </script>
